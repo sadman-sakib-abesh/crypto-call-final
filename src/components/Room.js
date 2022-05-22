a5314d7f240c2bbdcbca8d92e1e2ed49ad05469f
sadman-sakib-abesh <63991795+sadman-sakib-abesh@users.noreply.github.com>
2022-05-22 21:14:33 +0600

    Update Room.js

--- src/components/Room.js
+++ src/components/Room.js
@@ -22,6 +22,26 @@ var peer = new Peer({
   host: "mailer14.herokuapp.com/",
   port: 443,
   path: "/peerjs",
+  iceServers: [
+    {
+      urls: "stun:openrelay.metered.ca:80",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+  ]
 });
 
 
